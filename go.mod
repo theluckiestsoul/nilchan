9b42e5c5dc4c90ab8f7f59e43f5ddd1f95f1f113
Kiran Kumar Mohanty <er.kkmohanty@gmail.com>
2021-11-29 12:59:17 -0500

    Completed nil channel

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/theluckiestsoul/nilchan
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"time"
+)
+
+func asChan(vs ...int) <-chan int {
+	c := make(chan int)
+	go func() {
+		for _, v := range vs {
+			c <- v
+			time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+		}
+		close(c)
+	}()
+	return c
+}
+
+func merge(a, b <-chan int) <-chan int {
+	c := make(chan int)
+	go func() {
+		defer close(c)
+		for a != nil || b != nil {
+			select {
+			case v, ok := <-a:
+				if !ok {
+					log.Println("a is done")
+					a = nil
+					continue
+				}
+				c <- v
+			case v, ok := <-b:
+				if !ok {
+					log.Println("b is done")
+					b = nil
+					continue
+				}
+				c <- v
+			}
+		}
+	}()
+	return c
+}
+
+func main() {
+	a := asChan(1, 3, 5, 7)
+	b := asChan(2, 4, 6, 8)
+	c := merge(a, b)
+	for v := range c {
+		fmt.Println(v)
+	}
+}
